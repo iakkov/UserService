2bfe1d73b81428e23c8e22a3b28826be54a2c352
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-05-19 22:25:16 +0300

    SessionFactory класс

---
+++ Homework/src/main/java/ru/project/iakov/homework2/Runner.java
@@ -0,0 +1,16 @@
+package ru.project.iakov.homework2;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Runner {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
+            Session session = sessionFactory.openSession();
+        }
+    }
+}
