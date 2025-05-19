6ad1ab981e1eae7a4ab794667a800cea4e419189
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-05-19 22:27:49 +0300

    UserDao

---
+++ Homework/src/main/java/ru/project/iakov/homework2/UserDao.java
@@ -0,0 +1,12 @@
+package ru.project.iakov.homework2;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    void create(User user);
+    Optional<User> findById(Long id);
+    List<User> findAll();
+    void update(User user);
+    void delete(Long id);
+}
