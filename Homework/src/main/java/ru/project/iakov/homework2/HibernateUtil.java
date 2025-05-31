a20f05c2a80c4171c25b0c805e9655034014217d
iakovlysenko <lysenko_iakov@yahoo.com>
2025-06-01 02:28:42 +0300

    интеграционный тест

--- Homework/src/main/java/ru/project/iakov/homework2/HibernateUtil.java
+++ Homework/src/main/java/ru/project/iakov/homework2/HibernateUtil.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     @Getter
-    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
@@ -18,5 +18,21 @@ public class HibernateUtil {
             System.err.println(e.getMessage());
             throw new ExceptionInInitializerError(e);
         }
+    }
+
+    public static void rebuildSessionFactoryForTests() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        configuration.setProperty("hibernate.connection.url", System.getProperty("DB_URL"));
+        configuration.setProperty("hibernate.connection.username", System.getProperty("DB_USERNAME"));
+        configuration.setProperty("hibernate.connection.password", System.getProperty("DB_PASSWORD"));
+
+        configuration.addAnnotatedClass(User.class);
+
+        sessionFactory = configuration.buildSessionFactory();
     }
 }
