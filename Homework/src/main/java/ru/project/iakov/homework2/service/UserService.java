599c31d196b474cbdc4c4bad9d22270d58aedc5d
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-05-31 17:26:14 +0300

    UserService для удобства тестирования

---
+++ Homework/src/main/java/ru/project/iakov/homework2/service/UserService.java
@@ -0,0 +1,30 @@
+package ru.project.iakov.homework2.service;
+
+import ru.project.iakov.homework2.User;
+import ru.project.iakov.homework2.dao.UserDao;
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void createUser(User user) {
+        userDao.create(user);
+    }
+    public Optional<User> findById(Long id) {
+        return userDao.findById(id);
+    }
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+    public void update(User user) {
+        userDao.update(user);
+    }
+    public void delete(Long id) {
+        userDao.delete(id);
+    }
+}
