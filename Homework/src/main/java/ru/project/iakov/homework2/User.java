13eb0da28d789d43d528f26599515cfffbd6a38d
iakovlysenko <lysenko_iakov@yahoo.com>
2025-06-07 17:55:26 +0300

    тесты для дао слоя с контейнером

--- Homework/src/main/java/ru/project/iakov/homework2/User.java
+++ Homework/src/main/java/ru/project/iakov/homework2/User.java
@@ -17,8 +17,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private int age;
     private LocalDateTime createdAt;
 }
