0505a88a3266849f3fd87c655f3018ff5ca35b5d
iakovlysenko <lysenko_iakov@yahoo.com>
2025-06-30 00:48:16 +0300

    circuitBreaker

--- user-service/src/main/java/ru/project/iakov/homework2/service/KafkaProducerService.java
+++ user-service/src/main/java/ru/project/iakov/homework2/service/KafkaProducerService.java
@@ -3,6 +3,7 @@ package ru.project.iakov.homework2.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +14,13 @@ public class KafkaProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
     private static final String TOPIC = "user-events";
 
+    @CircuitBreaker(name = "kafkaProducer", fallbackMethod = "sendUserEventFallback")
     public void sendUserEvent(String message) {
         log.info("Отправка сообщения в Kafka: {}", message);
         kafkaTemplate.send(TOPIC, message);
+    }
+
+    public void sendUserEventFallback(String message, Throwable throwable) {
+        log.error("Ошибка отправки в Kafka: {}", throwable.getMessage());
     }
 }
